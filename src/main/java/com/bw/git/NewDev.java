64b052c43e6f05302a4f7962879b00736f5d7825
tarenawg <791565327@qq.com>
2020-03-20 09:09:15 +0800

    new branch

---
+++ src/main/java/com/bw/git/NewDev.java
@@ -0,0 +1,9 @@
+package com.bw.git;
+
+public class NewDev {
+    public static void main(String[] args) {
+        System.out.println("疫苗出现了，大家再也不用待在家里了");
+        System.out.println("呵呵呵！！！！！！！！！");
+    }
+
+}
--- src/main/java/com/bw/git/TestMain.java
+++ src/main/java/com/bw/git/TestMain.java
@@ -2,11 +2,11 @@ package com.bw.git;
 
 public class TestMain {
     public static void main(String[] args) {
-
         System.out.println("北网加油 北网加油  北网加油！！");
         System.out.println("武汉加油 北京加油  意大利加油！！！");
         System.out.println("四月份能开学吗，五一以后会开学！！！！！！！！");
         System.out.println("明天回更好！！！！！！！！！！！！！！！");
-        System.out.println("xxxxxxxxxxxxxxxxxxxxx")
+        System.out.println("yyyyyyyyyyyyyyyyy");
+        System.out.println("xxxxxxxxxxxxxxxxxxxxx");
     }
 }
